e17efdf836a76ea1e7d5698317b606fc3a8507e4
Zeztron <pateltharsh@gmail.com>
2023-03-20 09:28:06 -0500

    added prisma and nextauth

---
+++ src/lib/auth.ts
@@ -0,0 +1,49 @@
+import { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GoogleProvider from 'next-auth/providers/google';
+import { db } from '@/lib/db';
+import { getGoogleCredentials } from '@/lib/util';
+import { JWT } from 'next-auth/jwt';
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  providers: [GoogleProvider(getGoogleCredentials())],
+  callbacks: {
+    session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }): Promise<JWT> {
+      const dbUser = await db.user.findFirst({
+        where: { email: token.email },
+      });
+
+      if (!dbUser) {
+        token.id = user!.id;
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.picture,
+      } as JWT;
+    },
+    redirect() {
+      return '/dashboard';
+    },
+  },
+};
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -4,3 +4,21 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getGoogleCredentials(): {
+  clientId: string;
+  clientSecret: string;
+} {
+  const { GOOGLE_CLIENT_ID: clientId, GOOGLE_CLIENT_SECRET: clientSecret } =
+    process.env;
+
+  if (!clientId) {
+    throw new Error('GOOGLE_CLIENT_ID is not defined');
+  }
+
+  if (!clientSecret) {
+    throw new Error('GOOGLE_CLIENT_SECRET is not defined');
+  }
+
+  return { clientId, clientSecret };
+}
